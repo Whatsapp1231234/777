ecfda6dd955f44f47b48e82a638fd7a0745541d7
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-09-14 13:57:05 +0300

    канглд

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -240,7 +240,7 @@ await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() 
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}}
 
-handler.command = /^(menu|menú|memu|memú|help\?)$/i
+handler.command = /^(меню|menú|memu|memú|help\?)$/i
 
 export default handler
 

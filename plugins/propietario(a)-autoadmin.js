78b383aaed65150159a828df41819a5d1555cbad
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-09-14 15:45:52 +0300

    кенргр

--- plugins/propietario(a)-autoadmin.js
+++ plugins/propietario(a)-autoadmin.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw `*𝙔𝘼 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙈𝙄 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼)*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
-handler.command = /^admin.|atad|autoadmin$/i
+handler.command = /^admin.|atad|автоадмин$/i
 handler.rowner = true
 handler.botAdmin = true
 export default handler
--- plugins/propietario(a)-banchat.js
+++ plugins/propietario(a)-banchat.js
@@ -18,7 +18,7 @@ m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘽𝘼𝙉𝙀�
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = /^banchat|bangata|banchat2$/i
+handler.command = /^банчат|bangata|banchat2$/i
 //handler.botAdmin = true
 handler.rowner = true
 export default handler

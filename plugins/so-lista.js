3e4298f8397e2329fd26967c2c036a8a57d09af6
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-09-14 16:37:19 +0300

    Update so-lista.js

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -51,7 +51,7 @@ if (media === 'sticker') await conn.sendFile(m.chat, sticker, 'error.webp', '', 
 }
     
 if (chat.audios && m.text.match(/(админ)/gi)) {    
-let vn = '/media/админы.mp3'
+let vn = 'media/админы.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}
     

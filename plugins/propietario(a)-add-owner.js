ed7b18510cd9767a7b9651b01ec012aa8fdc87e4
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-09-14 15:33:40 +0300

    цкер

--- plugins/propietario(a)-add-owner.js
+++ plugins/propietario(a)-add-owner.js
@@ -2,12 +2,12 @@ const handler = async (m, { conn, text, args, usedPrefix, command }) => {
 const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
 if (!who) return conn.reply(m.chat,  `${lenguajeGB['smsAvisoMG']()} *𝙐𝙎𝘼𝙍 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙀𝙎𝙏𝘼 𝙁𝙊𝙍𝙈𝘼*\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊 : ${usedPrefix + command} @${m.sender.split('@')[0]}`,  m, {mentions: [m.sender]});
   switch (command) {
-case 'addowner':
+case 'добавитьсоздателя':
 const nuevoNumero = who;
 global.owner.push([nuevoNumero]);
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()} *𝙉𝙐𝙀𝙑𝙊 𝙉𝙐𝙈𝙀𝙍𝙊 𝘼𝙂𝙍𝙀𝙂𝘼𝘿𝙊 𝘾𝙊𝙉 𝙀𝙓𝙄𝙏𝙊𝙎 𝘾𝙊𝙈𝙊 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼)*`, m);
 break;
-case 'delowner':
+case 'удалитьсоздателя':
 const numeroAEliminar = who;
 const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
 if (index !== -1) {
@@ -18,6 +18,6 @@ await conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}*𝙀𝙇 𝙉𝙐𝙈�
 }
 break;
 }};
-handler.command = /^(добавитьсоздателя|delowner)$/i;
+handler.command = /^(добавитьсоздателя|удалитьсоздателя)$/i;
 handler.rowner = true;
 export default handler;
